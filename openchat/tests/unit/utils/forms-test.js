52f1633853a2bd901b0360d323ecf99f8d0716ce
Bartłomiej Bęczkowski <beczkowb@gmail.com>
2017-02-25 13:50:51 +0100

    missing tests

---
+++ openchat/tests/unit/utils/forms-test.js
@@ -0,0 +1,10 @@
+import forms from 'openchat/utils/forms';
+import { module, test } from 'qunit';
+
+module('Unit | Utility | forms');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = forms();
+  assert.ok(result);
+});
